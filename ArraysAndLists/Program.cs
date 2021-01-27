e371375fecb89a3e8c573d75925d508f94c1d415
d3lliott <d3elliott2@gmail.com>
2021-01-27 14:01:42 -0500

    done

--- ArraysAndLists/Program.cs
+++ ArraysAndLists/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ArraysAndLists
 {
@@ -6,6 +7,57 @@ namespace ArraysAndLists
     {
         static void Main(string[] args)
         {
+            var r = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            var evens = new List<int>();
+
+            var odds = new List<int>();
+            // method 1
+            //for (int i=0; i < r.Length; i++)
+            //{
+            //    if (r [i] % 2 ==00)
+            //    {
+            //        evens.Add(r[i]);
+            //    }
+            //    else
+            //    {
+            //        odds.Add(r[i]);
+            //    }
+
+            //}
+
+            //method 2, outputs the same as method 1
+            foreach (var num in r)
+            {
+                if (num % 2 == 0)
+                {
+                    evens.Add(num);
+                    //could Console.WriteLine here to eliminate second set of code
+                }
+                else
+                {
+                    odds.Add(num);
+                    //could Console.WriteLine here to eliminate second set of code
+                }
+            }
+            
+            //for loops that write out the numbers. the Console.WriteLine sections in these
+            // loops could just be moved into the earlier code to make the second set 
+            // of foreach (the ones below) unneccessary. 
+            foreach ( var i in evens)
+            {
+                Console.WriteLine($"{i} is an even number");
+            }
+            foreach (var i in odds)
+            {
+                Console.WriteLine($"{i} is an odd number");
+            }
+
+        
+
+
+
+            
             //TODO:
 
             // Create an int Array and populate numbers 1-10
